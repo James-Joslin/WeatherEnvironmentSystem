1a4c4c1f2b6cd75129c7dbd2441c6b4131e6dccc
James <74412992+James-Joslin@users.noreply.github.com>
2026-07-25 10:58:17 +0100

    build cs files and editor files

--- Source/WeatherEnvironmentSystemEditor/Public/WeatherEnvironmentSystemEditorModule.h
+++ Source/WeatherEnvironmentSystemEditor/Public/WeatherEnvironmentSystemEditorModule.h
@@ -4,9 +4,18 @@
 
 #include "Modules/ModuleManager.h"
 
+class IConsoleObject;
+
 class FWeatherEnvironmentSystemEditorModule : public IModuleInterface
 {
 public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+private:
+	void GenerateSkyboxMaterial();
+	void GenerateSkyDomeMaterial();
+
+	IConsoleObject* GenerateSkyboxMaterialCommand = nullptr;
+	IConsoleObject* GenerateSkyDomeMaterialCommand = nullptr;
 };
