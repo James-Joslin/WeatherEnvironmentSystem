2843436a61f85e6af68e405e2dce804cd3815071
James <74412992+James-Joslin@users.noreply.github.com>
2026-08-06 21:04:54 +0100

    debug comp update

---
+++ Source/WeatherEnvironmentSystem/Private/Components/WeatherGridDebugComponent.cpp
@@ -0,0 +1,9 @@
+// Copyright James Joslin. All Rights Reserved.
+
+#include "Components/WeatherGridDebugComponent.h"
+
+UWeatherGridDebugComponent::UWeatherGridDebugComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bAutoActivate = false;
+}
